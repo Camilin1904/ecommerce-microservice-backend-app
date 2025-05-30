e14f732e4dc1a5b9322512df461697bfdad4e414
Camilin <cami.car.val@outlook.com>
2025-05-29 23:52:31 -0500

    wait for services to go up

---
+++ k8s/delete-services.sh
@@ -0,0 +1,23 @@
+kubectl delete deployment api-gateway 
+kubectl delete deployment favourite-service 
+kubectl delete deployment order-service 
+kubectl delete deployment payment-service 
+kubectl delete deployment product-service 
+kubectl delete deployment proxy-client 
+kubectl delete deployment service-discovery 
+kubectl delete deployment shipping-service 
+kubectl delete deployment user-service 
+kubectl delete deployment zipkin 
+
+
+
+kubectl delete service api-gateway 
+kubectl delete service favourite-service 
+kubectl delete service order-service 
+kubectl delete service payment-service 
+kubectl delete service product-service 
+kubectl delete service proxy-client 
+kubectl delete service service-discovery 
+kubectl delete service shipping-service 
+kubectl delete service user-service 
+kubectl delete service zipkin 
